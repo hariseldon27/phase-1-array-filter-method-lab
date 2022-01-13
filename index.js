cc53580a9f0a5fc24a3f949ac28330f758894fd4
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-13 08:02:53 -0800

    commit tree 76c6ddd874881849ee9af36f1056eea0680eae23

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution here
+// write three functions
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(driversList, nameToSearch) {
+    // const matchFound = driversList.filter(driver => driver.toLowerCase() == nameToSearch.toLowerCase())
+    // console.log(matchFound)
+    // return matchFound
+    return driversList.filter(driver => driver.toLowerCase() == nameToSearch.toLowerCase())
+}
+
+//findMatching(['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'], 'bobbY')
+
+function fuzzyMatch(driversList, nameToSearch){
+    return driversList.filter(driver => driver.substring(0,2) === nameToSearch.substring(0,2))
+    //console.log(matchFound)
+    //return matchFound
+}
+//fuzzyMatch(['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'], 'Sarahann')
+
+
+function matchName(arrayOfDrivers, nameToSearch) {
+    return arrayOfDrivers.filter(driver => driver.name === nameToSearch)
+    console.log(matchFound)
+    //the above returns a new array, I just want to get the 
+    //hometown data element
+}
+
+matchName(drivers, 'Bobby')
